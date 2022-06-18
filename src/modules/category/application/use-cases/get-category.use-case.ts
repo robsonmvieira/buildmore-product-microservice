ad902e258bca3bb039553cd754763d58f26cee7f
Robson Maia <robsonmvieira@gmail.com>
2022-06-18 20:22:30 +0000

    feat(modules): :sparkles: create GetCategoryUseCase

---
+++ src/modules/category/application/use-cases/get-category.use-case.ts
@@ -0,0 +1,21 @@
+import { CategoryResponse, GetCategoryDTO } from "../../dtos";
+import { CategoryMapper } from "../../infra/database/category.mapper";
+import CategoryReposiory  from "../../infra/repository/i-category.repository";
+import { UseCase } from '../../../../core/application/use-cases'
+
+export class GetCategoryUseCase implements UseCase<GetCategoryDTO, CategoryResponse> {
+  constructor(
+    private readonly categoryRepository: CategoryReposiory.ICategoryRepository,
+    private readonly categoryMapper: CategoryMapper
+  ) {}
+
+  async execute(input: GetCategoryDTO): Promise<CategoryResponse>{
+
+    const category = await this.categoryRepository.findById(input.id);
+    const output = this.categoryMapper.fromOrmToOutput(category);
+    return output;
+  }
+}
+
+
+
