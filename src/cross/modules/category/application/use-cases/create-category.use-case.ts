14a7670954a074720075008a33a9ed3d98fb972c
Robson Maia <robsonmvieira@gmail.com>
2022-06-20 00:57:10 +0000

    build(infrastructure): :wrench: add config to descovery files from another project

--- src/cross/modules/category/application/use-cases/create-category.use-case.ts
+++ src/cross/modules/category/application/use-cases/create-category.use-case.ts
@@ -2,7 +2,7 @@ import { CategoryResponse, CreateCategoryDTO } from "../../dtos";
 import { Category } from "../../entities";
 import { CategoryMapper } from "../../infra/database/category.mapper";
 import CategoryReposiory  from "../../infra/repository/i-category.repository";
-import { UseCase } from '#core/application/use-cases'
+import { UseCase } from '../../../../core/application/use-cases'
 
 export class CreateCategoryUseCase implements UseCase<CreateCategoryDTO, CategoryResponse> {
   constructor(
